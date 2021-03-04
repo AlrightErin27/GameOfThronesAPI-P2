64e0db0bc2d4387f9b1a48d6f44078dbb3a06388
AlrightErin27 <erinmontybruce@gmail.com>
2021-03-04 15:55:51 -0800

    if perfect name is searched, results show a name that is a link to data in the api

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -10,29 +10,12 @@ router.post("/", async (req, res) => {
     const GotUrl = `https://www.anapioficeandfire.com/api/characters?name=${searchedName}`;
     const response = await axios.get(GotUrl);
     const characters = await response.data
-    console.log("👾",  characters[0].name );
+    // console.log("👾",  characters[0].name );
     res.render("results/results.ejs", { characters:characters });
   } catch (err) {
     console.log("🍎 🍎 🍎", err);
   }
 });
 
-//GET API INFO//  //in ejs file make squids for 'searchResults'
-// router.get("/", async (req, res) => {
-//   try {
-//     const GotUrl = "https://www.anapioficeandfire.com/api/characters/823";
-//     const response = await axios.get(GotUrl);
-//     console.log(response.data);
-//     const characters = response.data.name;
-//     console.log("👾 👾 👾 ", response.data);
-//     const searchResults = response.data.name;
-
-//     res.render("index", { characters: characters });
-//     res.render("/search", { searchResults: searchResults });
-//   } catch (err) {
-//     console.log("🍎 🍎 🍎", err);
-//     res.render("/search", { searchResults: "❗ Invalid search❗ " })
-//   }
-// });
 
 module.exports = router;
